688410595ebf32a2bfcb48fdd6d6ad05bd8402da
erikotsuda <erico.tsuda@gmail.com>
2016-04-15 13:14:49 +0900

    Adding http request task

--- app/src/main/java/com/wadaran/android/SituationActivity.java
+++ app/src/main/java/com/wadaran/android/SituationActivity.java
@@ -2,12 +2,31 @@ package com.wadaran.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SituationActivity extends AppCompatActivity {
+
+    public Button bGokon;
+    public Button bDate;
+    public Button bNomikai;
+    public AsyncHttpTask situationHttpTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_situation);
+
+        bGokon = (Button)findViewById(R.id.button_gokon);
+        bDate = (Button)findViewById(R.id.button_date);
+        bNomikai = (Button)findViewById(R.id.button_nomi);
+
+        bGokon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                situationHttpTask = new AsyncHttpTask(SituationActivity.this);
+                situationHttpTask.execute(Constants.S_GOKON);
+            }
+        });
     }
 }
