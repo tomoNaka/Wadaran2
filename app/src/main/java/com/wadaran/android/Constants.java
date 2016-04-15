ebb9159891ff8bd0c927fa200c38eb6f3ccd0afc
erikotsuda <erico.tsuda@gmail.com>
2016-04-15 13:25:39 +0900

    Adding http request task

---
+++ app/src/main/java/com/wadaran/android/Constants.java
@@ -0,0 +1,31 @@
+package com.wadaran.android;
+
+/**
+ * Created by erikotsuda on 4/15/16.
+ */
+public class Constants {
+
+    /**
+     * Server URL
+     */
+    static final String SERVER_URL = "http://1-dot-erikotestserver2.appspot.com/";
+
+    /**
+     * Server Application Name
+     */
+    static final String GET_LIST_BY_SITUATION = "testservletapp";
+
+    /**
+     * Param Names
+     */
+    static final String PARAM_NAME_SITUATION = "situation";
+
+    /**
+     * Situation name
+     */
+    static final String S_GOKON = "gokon";
+    static final String S_DATE = "date";
+    static final String S_NOMI = "nomi";
+
+
+}
